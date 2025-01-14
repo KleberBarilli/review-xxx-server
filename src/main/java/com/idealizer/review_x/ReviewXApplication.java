0063158f828c372558f4eb963e129810162e7a07
kleberbarilli <kleber.barilli73@gmail.com>
2025-01-14 14:21:25 -0300

    init project

---
+++ src/main/java/com/idealizer/review_x/ReviewXApplication.java
@@ -0,0 +1,13 @@
+package com.idealizer.review_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idealizer/review_x/ReviewXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idealizer.review_x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

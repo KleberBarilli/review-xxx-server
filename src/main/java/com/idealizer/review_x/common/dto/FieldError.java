bc3232396296cc4508f1e3ec409c9b02f059c3b5
kleberbarilli <kleber.barilli73@gmail.com>
2025-03-13 18:01:05 -0300

    execptions to do

---
+++ src/main/java/com/idealizer/review_x/common/dto/FieldError.java
@@ -0,0 +1,6 @@
+package com.idealizer.review_x.common.dto;
+
+public record FieldError(String field, String message) {
+
+
+}
--- src/main/java/com/idealizer/review_x/modules/actors/services/UpdateActorService.java
+++ src/main/java/com/idealizer/review_x/modules/actors/services/UpdateActorService.java
@@ -2,19 +2,21 @@ package com.idealizer.review_x.modules.actors.services;
 
 import com.idealizer.review_x.modules.actors.entities.Actor;
 import com.idealizer.review_x.modules.actors.repositories.ActorRepository;
+import com.idealizer.review_x.modules.actors.validators.ActorValidator;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 public class UpdateActorService {
     private final ActorRepository actorRepository;
+    private final ActorValidator actorValidator;
 
-    public UpdateActorService(ActorRepository actorRepository) {
+    public UpdateActorService(ActorRepository actorRepository, ActorValidator actorValidator) {
         this.actorRepository = actorRepository;
+        this.actorValidator = actorValidator;
     }
 
     public void execute(Actor actor) {
+        this.actorValidator.validate(actor);
         actorRepository.save(actor);
     }
 }

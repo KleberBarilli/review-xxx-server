7741a7bdf28e1715011def7be70c47df36a459bc
Kleber <33820115+KleberBarilli@users.noreply.github.com>
2025-07-21 21:17:40 -0300

    updated igdb games processor (#12)

---
+++ src/main/java/com/idealizer/review_x/application/modules/games/entities/GameMode.java
@@ -0,0 +1,24 @@
+package com.idealizer.review_x.application.modules.games.entities;
+
+public enum GameMode { SINGLE_PLAYER(1), MULTIPLAYER(2), CO_OPERATIVE(3), SPLIT_SCREEN(4), MASSIVELY_MULTIPLAYER_ONLINE(5), BATTLE_ROYALE(6), OTHER(-1);
+
+
+    private final int igdbId;
+
+    GameMode(int igdbId) {
+        this.igdbId = igdbId;
+    }
+
+    public int getIgdbId() {
+        return igdbId;
+    }
+
+    public static GameMode fromIgdbId(int id) {
+        for (GameMode mode : values()) {
+            if (mode.igdbId == id) {
+                return mode;
+            }
+        }
+        return OTHER;
+    }
+}

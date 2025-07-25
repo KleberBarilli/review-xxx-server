76c3e70ceb12425f7b516623d409ce640eb84d64
kleber.barilli <kleber.barilli73@gmail.com>
2025-07-25 14:38:57 -0300

    wip

--- src/main/java/com/idealizer/review_x/config/SecurityConfiguration.java
+++ src/main/java/com/idealizer/review_x/config/SecurityConfiguration.java
@@ -14,29 +14,26 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                // .formLogin(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> {
-                    // authorize.requestMatchers("/login/**").permitAll();
-                    authorize.anyRequest().permitAll();
-                })
-                .oauth2Login(Customizer.withDefaults())
-                .build();
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+        //http.formLogin(withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> {
-            web.ignoring().requestMatchers(
-                    "/v2/api-docs",
-                    "/v3/api-docs/**",
-                    "/swagger-resources/**",
-                    "/swagger-ui/**",
-                    "/webjars/**",
-                    "/actuator/**");
-        };
-    }
+//
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return web -> {
+//            web.ignoring().requestMatchers(
+//                    "/v2/api-docs",
+//                    "/v3/api-docs/**",
+//                    "/swagger-resources/**",
+//                    "/swagger-ui/**",
+//                    "/webjars/**",
+//                    "/actuator/**");
+//        };
+//    }
 
 }

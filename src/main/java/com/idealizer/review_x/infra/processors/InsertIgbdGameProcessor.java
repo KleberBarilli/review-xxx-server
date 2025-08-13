0f4b8a65cfdde6c597bf7b30143abc5f9ddaf076
kleber.barilli <kleber.barilli73@gmail.com>
2025-08-13 12:45:08 -0300

    .

--- src/main/java/com/idealizer/review_x/infra/processors/InsertIgbdGameProcessor.java
+++ src/main/java/com/idealizer/review_x/infra/processors/InsertIgbdGameProcessor.java
@@ -61,7 +61,7 @@ public class InsertIgbdGameProcessor {
         return provider.getAccessToken();
     }
 
-    @Scheduled(cron = "0 59 02 * * *", zone = "America/Sao_Paulo")
+    @Scheduled(cron = "0 13 00 * * *", zone = "America/Sao_Paulo")
 
     public void importGames() {
 
